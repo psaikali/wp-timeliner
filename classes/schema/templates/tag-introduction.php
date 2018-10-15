b122cc74647860b654e3b2fb0d0a8355a9b92551
Pierre Saïkali <dev@mosaika.fr>
2018-10-15 17:03:55 +0200

    Template files to display introduction text before taxonomy add forms.

--- classes/schema/templates/tag-introduction.php
+++ classes/schema/templates/tag-introduction.php
@@ -1,5 +1,8 @@
+<?php if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} ?>
 <div class="wpt schema-introduction tag">
 	<p>
-		<?php _e( 'Tags offer a good way to group achievements by common topics, just like native WordPress post tags.', 'wp-timeliner' ); ?>
+		<?php _e( 'Tags are useful to group and filter achievements. Some tag examples for grouping achievements could be: a skill (in a resume), a country (for a roadtrip) or a phase (on a crowdfunding page).', 'wp-timeliner' ); ?>
 	</p>
 </div>
