4b51c9ecc1ce90ee9f6e764846507bac0686ac01
Pierre Saïkali <dev@mosaika.fr>
2018-10-15 16:53:17 +0200

    Optimizing the CarbonFields inclusion.

--- classes/timeliner.php
+++ classes/timeliner.php
@@ -22,7 +22,7 @@ class Timeliner implements Has_Hooks {
 	 * @return void
 	 */
 	public function fire() {
-		$this->include_libraries();
+		//$this->include_libraries();
 		$hooks = new Setup\Register_Hooks( __NAMESPACE__ );
 		//$assets = new Setup\Enqueue_Assets();
 	}
@@ -31,7 +31,7 @@ class Timeliner implements Has_Hooks {
 	 * Register required hooks.
 	 */
 	public function hooks() {
-		add_action( 'after_setup_theme', [ '\Carbon_Fields\Carbon_Fields', 'boot' ] );
+		add_action( 'after_setup_theme', [ $this, 'boot_carbon_fields' ] );
 	}
 
 
@@ -58,12 +58,25 @@ class Timeliner implements Has_Hooks {
 	 */
 	protected function include_libraries() {
 		// Include CarbonFields library if it does not exist.
+		// if ( ! class_exists( '\Carbon_Fields' ) ) {
+		// 	require_once TIMELINER_DIR . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'carbon-fields-plugins' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+		// 	require_once TIMELINER_DIR . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'carbon-fields-icon-field' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+		// }
+
+		// // Register a new CarbonFields Icon field type.
+		// require_once TIMELINER_DIR . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'carbon-fields-icon-field' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'Icon_Field.php';
+		// require_once TIMELINER_DIR . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'carbon-fields-icon-field' . DIRECTORY_SEPARATOR . 'field.php';
+	}
+
+	public function boot_carbon_fields() {
 		if ( ! class_exists( '\Carbon_Fields' ) ) {
-			require_once TIMELINER_DIR . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'carbon-fields-plugins' . DIRECTORY_SEPARATOR . 'carbon-fields-plugin.php';
+			require_once TIMELINER_DIR . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'carbon-fields-plugins' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 		}
 
-		// Register a new CarbonFields Icon field type.
-		require_once TIMELINER_DIR . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'carbon-fields-icon-field' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'Icon_Field.php';
-		require_once TIMELINER_DIR . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'carbon-fields-icon-field' . DIRECTORY_SEPARATOR . 'field.php';
+		if ( ! class_exists( '\Carbon_Field_Icon\Icon_Field' ) ) {
+			require_once TIMELINER_DIR . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'carbon-fields-icon-field' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+		}
+
+		\Carbon_Fields\Carbon_Fields::boot();
 	}
 }
