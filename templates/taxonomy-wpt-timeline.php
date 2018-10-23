c8b3290438ba919a97df5b8a2614b32641791e27
Pierre Saïkali <dev@mosaika.fr>
2018-10-23 18:29:06 +0200

    Adding a container on the Timeline taxonomy template page.

--- templates/taxonomy-wpt-timeline.php
+++ templates/taxonomy-wpt-timeline.php
@@ -8,17 +8,19 @@ do_action( 'wpt.template.before-header', $timeline );
 get_header();
 do_action( 'wpt.template.before-loop', $timeline );
 
-var_dump(
-	$timeline->get_theme()
-);
-
 if ( have_posts() ) {
+	?>
+	<section class="wpt-timeline wpt-theme-<?php echo esc_attr( $timeline->get_theme_slug() ); ?>">
+	<?php
 	while ( have_posts() ) {
 		the_post();
 		do_action( 'wpt.template.start-loop-item', $timeline );
 		Templates::get_template( 'content-wpt-achievement.php', [ 'timeline' => $timeline, 'achievement' => $post->achievement ] );
 		do_action( 'wpt.template.end-loop-item', $timeline );
 	}
+	?>
+	</section>
+	<?php
 } else {
 	do_action( 'wpt.template.before-no-achievement-found', $timeline );
 	Templates::get_template( 'no-achievement-found.php', [ 'timeline' => $timeline ] );
