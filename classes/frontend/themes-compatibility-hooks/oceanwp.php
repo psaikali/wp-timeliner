9e1194baafe22015c7dadb70cb80838e0a41007a
Pierre Saïkali <dev@mosaika.fr>
2018-10-23 18:33:25 +0200

    Adding compatibility markup with OceanWP

---
+++ classes/frontend/themes-compatibility-hooks/oceanwp.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WP_Timeliner\Compatibility\OceanWP;
+
+function before_loop_markup() {
+	?>
+	<?php do_action( 'ocean_before_content_wrap' ); ?>
+	<div id="content-wrap" class="container clr">
+		<?php do_action( 'ocean_before_primary' ); ?>
+		<div id="primary" class="content-area clr">
+			<?php do_action( 'ocean_before_content' ); ?>
+			<div id="content" class="site-content clr">
+				<?php do_action( 'ocean_before_content_inner' ); ?>
+	<?php
+}
+add_action( 'wpt.template.before-loop', __NAMESPACE__ . '\before_loop_markup' );
+
+function after_loop_markup() {
+	?>
+				<?php do_action( 'ocean_after_content_inner' ); ?>
+			</div><!-- #content -->
+			<?php do_action( 'ocean_after_content' ); ?>
+		</div><!-- #primary -->
+		<?php do_action( 'ocean_after_primary' ); ?>
+	<?php do_action( 'ocean_display_sidebar' ); ?>
+	</div><!-- #content-wrap -->
+	<?php do_action( 'ocean_after_content_wrap' ); ?>
+	<?php
+}
+add_action( 'wpt.template.after-loop', __NAMESPACE__ . '\after_loop_markup' );
